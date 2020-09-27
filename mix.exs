701a81b354bc09344125bb027e71d8e99b19895a
kuunamaka <mauisahashi@gmail.com>
2020-09-27 16:26:07 +0900

    mix.exs is updated

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule TodoTutorial.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:mock, "~> 0.3.0", only: :test}
     ]
   end
 
