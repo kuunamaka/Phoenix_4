af23056a664d28f684466bd972813e6ac4f0e75c
kuunamaka <mauisahashi@gmail.com>
2020-10-02 20:35:42 +0900

    Update accountes.ex

--- lib/todo_tutorial/accounts/user.ex
+++ lib/todo_tutorial/accounts/user.ex
@@ -7,12 +7,13 @@ defmodule TodoTutorial.Accounts.User do
   schema "users" do
     field :name, :string
     field :username, :string
-    
+
     timestamps()
   end
 
   # 新しいuser作成時のみに確認する事項
-  def changeset(user, attrs) do # Accepts Accounts.User struct and attributes
+  # Accepts Accounts.User struct and attributes
+  def changeset(user, attrs) do
     user
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
