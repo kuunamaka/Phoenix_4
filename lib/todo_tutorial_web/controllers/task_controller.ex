40c20447ebbbbb776d7e2e37e0dc48381a6b374d
kuunamaka <mauisahashi@gmail.com>
2020-10-03 23:04:30 +0900

    Update Todo to Todos

--- lib/todo_tutorial_web/controllers/task_controller.ex
+++ lib/todo_tutorial_web/controllers/task_controller.ex
@@ -1,23 +1,23 @@
 defmodule TodoTutorialWeb.TaskController do
   use TodoTutorialWeb, :controller
 
-  alias TodoTutorial.Todo
-  alias TodoTutorial.Todo.Task
+  alias TodoTutorial.Todos
+  alias TodoTutorial.Todos.Task
 
   plug :load_assigned when action in [:new, :create, :edit, :update]
 
   def index(conn, _params) do
-    tasks = Todo.list_tasks()
+    tasks = Todos.list_tasks()
     render(conn, "index.html", tasks: tasks)
   end
 
   def new(conn, _params) do
-    changeset = Todo.change_task(%Task{})
+    changeset = Todos.change_task(%Task{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"task" => task_params}) do
-    case Todo.create_task(task_params) do
+    case Todos.create_task(task_params) do
       {:ok, _} ->
         conn
         |> put_flash(:info, "Task created successfully.")
@@ -29,20 +29,20 @@ defmodule TodoTutorialWeb.TaskController do
   end
 
   def show(conn, %{"id" => id}) do
-    task = Todo.get_task!(id)
+    task = Todos.get_task!(id)
     render(conn, "show.html", task: task)
   end
 
   def edit(conn, %{"id" => id}) do
-    task = Todo.get_task!(id)
-    changeset = Todo.change_task(task)
+    task = Todos.get_task!(id)
+    changeset = Todos.change_task(task)
     render(conn, "edit.html", task: task, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "task" => task_params}) do
-    task = Todo.get_task!(id)
+    task = Todos.get_task!(id)
 
-    case Todo.update_task(task, task_params) do
+    case Todos.update_task(task, task_params) do
       {:ok, _} ->
         conn
         |> put_flash(:info, "Task is updated successfully!")
@@ -54,15 +54,15 @@ defmodule TodoTutorialWeb.TaskController do
   end
 
   def delete(conn, %{"id" => id}) do
-    task = Todo.get_task!(id)
-    {:ok, _task} = Todo.delete_task(task)
+    task = Todos.get_task!(id)
+    {:ok, _task} = Todos.delete_task(task)
 
     conn
     |> put_flash(:info, "Task deleted successfully.")
     |> redirect(to: Routes.task_path(conn, :index))
   end
 
-  def load_assigned(conn, _) do
-    assign(conn, :assigned, Todo.list_alphabetical_assigned())
+  defp load_assigned(conn, _) do
+    assign(conn, :assigned, Todos.list_alphabetical_assigned())
   end
 end
