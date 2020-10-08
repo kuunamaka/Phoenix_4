5fbcc40e0fd2ee1b6b35a5eff786326e77339027
kuunamaka <mauisahashi@gmail.com>
2020-10-08 12:09:45 +0900

    Update assign.ex

--- lib/todo_tutorial/todo/assign.ex
+++ lib/todo_tutorial/todo/assign.ex
@@ -1,10 +1,11 @@
 defmodule TodoTutorial.Todos.Assignee do
   use Ecto.Schema
   import Ecto.Changeset
-  alias TodoTutorial.Todos.Task
 
   schema "assigned" do
     field :name, :string
+
+    has_many :tasks, TodoTutorial.Todos.Task
 
     timestamps()
   end
