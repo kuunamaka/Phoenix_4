5994ed600621f99166a1af8133fcaa20fcdb8211
kuunamaka <mauisahashi@gmail.com>
2020-10-09 20:40:12 +0900

    Add doc

--- lib/todo_tutorial_web/controllers/page_controller.ex
+++ lib/todo_tutorial_web/controllers/page_controller.ex
@@ -1,6 +1,9 @@
 defmodule TodoTutorialWeb.PageController do
   use TodoTutorialWeb, :controller
 
+  @doc """
+  A function for rendering `index.html`
+  """
   def index(conn, _params) do
     render(conn, "index.html")
   end
