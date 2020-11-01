9b284e8d756d78a4ea68c00877910a4d31cc1d48
Kuunamaka <mauisahashi@gmail.com>
2020-11-01 18:27:43 +0900

    Update comment on create_fav_task()

--- lib/todo_tutorial_web/controllers/task/favorite_controller.ex
+++ lib/todo_tutorial_web/controllers/task/favorite_controller.ex
@@ -7,14 +7,16 @@ defmodule TodoTutorialWeb.Task.FavoriteController do
   alias TodoTutorial.Todos.Accounts
    
   @doc """
-  the `id` inside of create() is from url that shown below
+  The `id` inside of create() is from url that shown below
   so it's a id from task:
   `PATCH/PUT /api/tasks/:id`
+
+  l17; defining the regulation user for liking the task (user → "Maui")
   """
   def create(conn, %{"task_id" => task_id}) do
     user = Accounts.get_user_by(name: "Maui")
     task = Todos.get_task!(task_id)
-    likes = Todos.create_favorite_task(task.id, user.id)
+    likes = Todos.create_favorite_task(task, user.id)
     render(conn, "index.html", task: task, likes: likes)
   end
 end
