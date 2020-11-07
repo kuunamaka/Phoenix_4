b6114c0f47a8a692ba683224fef8e8c5dd8ed0ad
Kuunamaka <mauisahashi@gmail.com>
2020-11-07 21:26:10 +0900

    NEEDS TO UPDATE

--- lib/todo_tutorial_web/views/task_view.ex
+++ lib/todo_tutorial_web/views/task_view.ex
@@ -2,9 +2,13 @@ defmodule TodoTutorialWeb.TaskView do
   use TodoTutorialWeb, :view
 
   @doc """
-  A function for the selection when adding a new task
+  A function for selecting the assigning user when adding a task
   """
   def assign_select_options(assigned) do
     for assign <- assigned, do: {assign.name, assign.id}
   end
+
+  @doc """
+  need to write the function for finding the id for FavoritedTask by next Friday
+  """
 end
