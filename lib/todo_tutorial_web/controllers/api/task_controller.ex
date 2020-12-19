0100aadbf0514d7c837bff313693501d250fb9e2
Kuunamaka <mauisahashi@gmail.com>
2020-12-19 18:07:36 +0900

    Add api controllers

---
+++ lib/todo_tutorial_web/controllers/api/task_controller.ex
@@ -0,0 +1,43 @@
+defmodule TodoTutorialWeb.Api.TaskController do
+  use TodoTutorialWeb, :controller
+
+  alias TodoTutorial.Todos
+  alias TodoTutorial.Todos.Task
+
+  action_fallback TodoTutorialWeb.FallbackController
+
+  def index(conn, _params) do
+    tasks = Todos.list_tasks()
+    render(conn, "index.json", tasks: tasks)
+  end
+
+  def create(conn, %{"task" => task_params}) do
+    with {:ok, %Task{} = task} <- Todos.create_task(task_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.api_task_path(conn, :show, task))
+      |> render("show.json", task: task)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    task = Todos.get_task!(id)
+    render(conn, "show.json", task: task)
+  end
+
+  def update(conn, %{"id" => id, "task" => task_params}) do
+    task = Todos.get_task!(id)
+
+    with {:ok, %Task{} = task} <- Todos.update_task(task, task_params) do
+      render(conn, "show.json", task: task)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    task = Todos.get_task!(id)
+
+    with {:ok, %Task{}} <- Todos.delete_task(task) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
