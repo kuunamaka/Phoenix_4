a6ae6accfd71c278ce72bea752dbeb1293af3834
Kuunamaka <mauisahashi@gmail.com>
2020-12-21 16:11:06 +0900

    Changed comments to ENG and tried to display data to its browser but not reactivly working

--- assets/js/src/components/UserList.vue
+++ assets/js/src/components/UserList.vue
@@ -1,12 +1,25 @@
-<!-- ユーザーを表示する基盤のところ -->
+<!-- basement code for the listing user page -->
 <template>
-  <table>
-    <p>inside of UserList component</p>  
+   <table>
+    <p>inside of UserList component!</p>  
+    <h2>{{ users.id }}</h2>
+    <pre>{{ users.name }}</pre>
   </table>
 </template>
 
 <script>
-  export default {
+export default {
   name: 'userList'
 }
+var vm = new Vue({
+  el: '#app',
+  data: {
+    users: []
+  },
+  created: function(){
+    axios.get('/users').then(function(response){
+      app.users = response.data.data;
+    })
+  }
+})
 </script>
